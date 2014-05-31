51eb0dfc74083e592f907b97bab529f3140c1abd
azu <azuciao@gmail.com>
2014-05-31 21:30:56 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# asciidoc-dependency-graph
+
+Generate dependency-graph JSON tree from [AsciiDoc](http://www.methods.co.nz/asciidoc/ "AsciiDoc").
+
+## Installation
+
+``` sh
+npm install asciidoc-dependency-graph
+```
+
+## Usage
+
+``` js
+var adocDependency = new AsciidocDependency({
+    "cwd": __dirname,
+    "pattern": "fixtures/*.adoc"
+});
+adocDependency.parse().then(function (array) {
+    console.log(array);
+}).catch(console.error.bind(console));
+// or
+adocDependency.parse(function(error,array){
+    console.log(array);
+});
+/*
+[
+{ filePath: '/Users/azu/Dropbox/workspace/JavaScript/library/asciidoc-dependency-graph/test/fixtures/x.adoc',
+    ids: [ 'x-doc' ],
+    referenceIds: [] },
+{ filePath: '/Users/azu/Dropbox/workspace/JavaScript/library/asciidoc-dependency-graph/test/fixtures/y.adoc',
+    ids: [ 'y-doc', 'embed-y-code' ],
+    referenceIds: [ 'x-doc' ] }
+]
+*/
+```
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## License
+
+MIT
---
+++ test/asciidoc-dependency-test.js
@@ -0,0 +1,121 @@
+/**
+ * Created by azu on 2014/05/30.
+ * LICENSE : MIT
+ */
+"use strict";
+
+var _ = require("lodash");
+var pather = require("path");
+var assert = require("power-assert");
+var AsciidocDependency = require("../lib/asciidoc-dependency");
+var shouldFulfilled = require("promise-test-helper").shouldFulfilled;
+var shouldRejected = require("promise-test-helper").shouldRejected;
+describe("AsciidocDependency", function () {
+    var adocDependency = new AsciidocDependency({
+        "cwd": __dirname,
+        "pattern": "fixtures/*.adoc"
+    });
+    describe("#fileListPromise", function () {
+        it("should return fileList promise", function () {
+            var promises = adocDependency._fileListPromise();
+            return promises.then(function (fileList) {
+                assert(fileList.length === 2);
+            });
+        });
+    });
+    describe("#pickUpIDs", function () {
+        context("when has not id", function () {
+            var doc = "test document";
+            it("should return empty array", function () {
+                var results = adocDependency.pickUpIDs(doc);
+                assert(results.length === 0);
+            });
+        });
+        context("when has [[id]]", function () {
+            var ids = ["id", "test"];
+            var doc = "[[id]]\ntest\n[[test]] document";
+            it("should return array of ids", function () {
+                var results = adocDependency.pickUpIDs(doc);
+                assert(results.length === 2);
+                assert(results[0] === ids[0]);
+                assert(results[1] === ids[1]);
+            });
+        });
+    });
+    describe("#pickUpLinks", function () {
+        context("when has not link", function () {
+            var doc = "test document";
+            it("should return empty array", function () {
+                var results = adocDependency.pickUpLinks(doc);
+                assert(results.length === 0);
+            });
+        });
+        context("when has <<id>>", function () {
+            var ids = ["id"];
+            var doc = "<<id>>";
+            it("should return array of ids", function () {
+                var results = adocDependency.pickUpLinks(doc);
+                assert(results.length === ids.length);
+                assert(results[0] === ids[0]);
+            });
+        });
+        context("when has <<id,text>>", function () {
+            var ids = ["id"];
+            var doc = "<< id , text>>";
+            it("should return array of ids", function () {
+                var results = adocDependency.pickUpLinks(doc);
+                assert(results.length === ids.length);
+                assert(results[0] === ids[0]);
+            });
+        });
+        context("when has a number of <<id>>", function () {
+            var ids = ["id", "日本語"];
+            var doc = "<< id , text>> test is <<日本語, nihongo>>";
+            it("should return array of ids", function () {
+                var results = adocDependency.pickUpLinks(doc);
+                assert(results.length === ids.length);
+                assert(results[0] === ids[0]);
+                assert(results[1] === ids[1]);
+            });
+        });
+    });
+    describe("#parse", function () {
+        it("should return object", function () {
+            return shouldFulfilled(adocDependency.parse()).then(function (result) {
+                assert(Array.isArray(result));
+                assert(result.length === 2);
+            });
+        });
+        it("object has fileName as key", function () {
+            return shouldFulfilled(adocDependency.parse()).then(function (object) {
+                var fileNameList = _.pluck(object, "filePath").map(function (filePath) {
+                    return pather.basename(filePath);
+                });
+                assert(_.contains(fileNameList, "x.adoc"));
+                assert(_.contains(fileNameList, "y.adoc"));
+            });
+        });
+        context("About x.adoc", function () {
+            var yObject;
+            before(function (done) {
+                return adocDependency.parse().then(function (array) {
+                    yObject = _.find(array, function (object) {
+                        return pather.basename(object.filePath) === "y.adoc";
+                    });
+                    done();
+                });
+            });
+            it("object has 'ids' and 'referenceIds'", function () {
+                assert(Array.isArray(yObject.ids));
+                assert(Array.isArray(yObject.referenceIds));
+            });
+            it("has [[y-doc]] and [[embed-y-code]]", function () {
+                assert(_.contains(yObject.ids, "y-doc"));
+                assert(_.contains(yObject.ids, "embed-y-code"));
+            });
+            it("reference [[x-doc]]", function () {
+                assert(_.contains(yObject.referenceIds, "x-doc"));
+            });
+        });
+    });
+});
